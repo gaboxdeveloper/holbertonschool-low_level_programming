3908df49401052b22ed22312e14a0977c0813295
Gabriel <grossy.sjb@gmail.com>
2023-06-15 05:53:50 -0700

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * Description: This program is to print a message
+ * using the Betty coding style
+ * Return: 0
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
