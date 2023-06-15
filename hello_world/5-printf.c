c3281006ecdb6456d50e2542af7549bd7604ec40
Gabriel <grossy.sjb@gmail.com>
2023-06-15 06:00:34 -0700

    task5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: This program is to print a message
+ * using the Betty coding style
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
