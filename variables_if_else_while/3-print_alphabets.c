7957a5fb772ff22ec7b6ecf8ad208f45bd3197b3
Gabriel <grossy.sjb@gmail.com>
2023-06-15 09:39:11 -0700

    task3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * description: variables, if, else and while
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int lc, uc;
+
+	for (lc = 97; lc < 123; lc++)
+		putchar(lc);
+	for (uc = 65; lc < 91; lc++)
+		putchar(uc);
+	putchar(10);
+	return (0);
+}
