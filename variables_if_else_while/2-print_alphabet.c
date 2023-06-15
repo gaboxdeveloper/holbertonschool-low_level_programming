69f76709994d027e075e82940fa0401689cdccf7
Gabriel <grossy.sjb@gmail.com>
2023-06-15 09:24:13 -0700

    tassk2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * description: variables, if, else and while
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar('a' + 1);
+	}
+
+	return (0);
+}
