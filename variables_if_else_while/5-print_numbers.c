2039a9e1e05ba718d4b4b2f7dcc33cd916485aa0
Gabriel <grossy.sjb@gmail.com>
2023-06-15 10:22:54 -0700

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * description: variables, if, else and while
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+		printf("%d", numbers);
+	printf("\n");
+	return (0);
+}
