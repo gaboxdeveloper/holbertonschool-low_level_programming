ce1c080f30aba5e7dafacb4453bdb7d15e6fa8c7
Gabriel <grossy.sjb@gmail.com>
2023-06-15 06:16:29 -0700

    task6

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: This program is to print a message
+ * using the Betty coding style
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
