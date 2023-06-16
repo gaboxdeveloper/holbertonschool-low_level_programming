49d048cd3b46769534cfa4fdb743dd1bea8b3727
Gabriel <grossy.sjb@gmail.com>
2023-06-16 05:37:12 -0700

    task9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * description: variables, if, else and while
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
