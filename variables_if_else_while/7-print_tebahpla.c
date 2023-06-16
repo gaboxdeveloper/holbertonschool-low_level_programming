a753ab9ba16fab002459c0c3ea231c52b5ce91dc
Gabriel <grossy.sjb@gmail.com>
2023-06-16 05:25:42 -0700

    task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * description: variables, if, else and while
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
