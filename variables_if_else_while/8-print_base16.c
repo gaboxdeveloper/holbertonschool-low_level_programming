863f519fb13ab0b9d01c807f9e979a7742860bea
Gabriel <grossy.sjb@gmail.com>
2023-06-16 05:30:54 -0700

    task8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * description: variables, if, else and while
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int hx, lc;
+
+	for (hx = 48; hx < 58; hx++)
+		putchar(hx);
+	for (lc = 97; lc < 123; lc++)
+		putchar(lc);
+	putchar(10);
+	return (0);
+}
