12c121627cc08641c2fe4f0e7d1caa6524c3f03a
Gabriel <grossy.sjb@gmail.com>
2023-06-16 05:22:11 -0700

    task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * description: variables, if, else and while
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 48; numbers < 58; numbers++)
+		putchar(numbers);
+	putchar(10);
+	return (0);
+}
