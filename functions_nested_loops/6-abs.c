8202a3c55e45dab58bde9428e405f841b63dded8
Gabriel <grossy.sjb@gmail.com>
2023-06-18 08:53:27 -0700

    task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - function
+ *
+ * @n: asdasd
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	unsigned int a;
+
+	if (n > 0)
+		_putchar(n);
+	else if (n < 0)
+	{
+		a = n;
+		_putchar(a);
+	}
+	else
+		_putchar(n);
+	return (0);
+}
