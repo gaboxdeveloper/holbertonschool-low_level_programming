5effcd747068821f1dd702e423ca236631f2b4f6
Gabriel <grossy.sjb@gmail.com>
2023-06-19 06:19:13 -0700

    task9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,22 +8,26 @@
 
 void times_table(void)
 {
-	int n, x, p;
+	int num, mult, prod;
 
-	for (n = 0; n < 10; n++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (x = 0; x < 10; x++)
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
-			p = x * n;
 
-			if (p <= 9)
+			prod = num * mult;
+
+			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((p / 10) + '0');
-			_putchar((p % 10) + '0');
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
-		_putchar(10);
+		_putchar('\n');
 	}
 }
