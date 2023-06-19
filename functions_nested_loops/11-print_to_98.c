84698f9d081e12e56c6af643086479af351473b1
Gabriel <grossy.sjb@gmail.com>
2023-06-19 06:47:26 -0700

    task11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,25 @@
 void print_to_98(int n)
 {
 	int a;
+
 	if (n < 98)
 	{
 		for (a = n; a < 99; a++)
+		{
 			printf("%d", a);
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	else if (n > 98)
 	{
 		for (a = n; a > 97; a--)
+		{
 			printf("%d", a);
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	else
+		printf("%d", 98);
 }
