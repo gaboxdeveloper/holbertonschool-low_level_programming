e907b17c5d3aff31ab857ac95bc57823aa49ab3d
Gabriel <grossy.sjb@gmail.com>
2023-06-19 10:46:38 -0700

    task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function to know if its uppercase
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int p = a * b;
+
+	return (p);
+}
