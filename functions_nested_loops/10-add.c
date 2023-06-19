8c134bee98454dc5abc73fdc3b4a810045cbb4c2
Gabriel <grossy.sjb@gmail.com>
2023-06-19 06:25:20 -0700

    task10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - function
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int p;
+
+	p = a + b;
+
+	return (p);
+}
