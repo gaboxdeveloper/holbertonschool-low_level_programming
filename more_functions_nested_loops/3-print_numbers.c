e7aa95473c5147d80c09d34604257b0f6457d077
Gabriel <grossy.sjb@gmail.com>
2023-06-19 10:52:23 -0700

    task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function
+ *
+ * Return: 0
+ */
+
+int print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
