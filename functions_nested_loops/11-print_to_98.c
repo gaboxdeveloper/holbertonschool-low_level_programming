61db33c3a2cf7b9b666577916265cf38ef22d3b9
Gabriel <grossy.sjb@gmail.com>
2023-06-19 06:39:25 -0700

    task11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function
+ *
+ * @n: parameter 2
+ *
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int a;
+
+	for (a = n; a < 99; a++)
+		printf("%d", a);
+}
