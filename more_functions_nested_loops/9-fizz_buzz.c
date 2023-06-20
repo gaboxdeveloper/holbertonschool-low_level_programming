9c58f5d33ffbb4293fee380725cd6bd5fb0cfd91
Gabriel <grossy.sjb@gmail.com>
2023-06-20 06:13:28 -0700

    task9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if ((i % 5) == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else if ((i % 5) && (i % 3) == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
 	return (0);
 }
