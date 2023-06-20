3104c2637cde4547880f3ef9c13ec3572e728b91
Gabriel <grossy.sjb@gmail.com>
2023-06-20 06:58:33 -0700

    task7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function
+ * @n: paramter
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(' ');
+	_putchar(92);
+	_putchar('\n');
+}
