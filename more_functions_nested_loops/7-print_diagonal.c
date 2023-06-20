4db521da3209028b360621b95001edf7298a2d48
Gabriel <grossy.sjb@gmail.com>
2023-06-20 09:55:28 -0700

    task7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,11 +13,11 @@ void print_diagonal(int n)
 	if (n <= 0)
 		_putchar('\n');
 	else
-		for (i = 1; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (x = 1; x <= i; x++)
+			for (x = 0; x < i; x++)
 			{
-				if (x != 1)
+				if (x != 0)
 					_putchar(' ');
 			}
 			_putchar(92);
