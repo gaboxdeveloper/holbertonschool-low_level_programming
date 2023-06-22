aca4965c054a2a00d7860b2d4faab8bdfa6a84ae
Gabriel <grossy.sjb@gmail.com>
2023-06-22 06:35:46 -0700

    task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: pointer char s
+ * Return: a
+ */
+
+int _strlen(char *s)
+{
+	int a, count;
+
+	for (a = 0; a < *s; a++)
+	{
+		while (count != '\0')
+			count++;
+	}
+	return (count);
+}
