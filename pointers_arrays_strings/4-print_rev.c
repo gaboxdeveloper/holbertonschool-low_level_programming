15da4aa0b56b393aa02a0e06412cbb7dd957bb24
Gabriel <grossy.sjb@gmail.com>
2023-06-22 10:54:23 -0700

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = s['\0']; s[i] != 0; i--)
+		_putchar(s[i]);
+}
