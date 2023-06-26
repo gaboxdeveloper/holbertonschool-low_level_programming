7ede4879de799fe4a93ec9bbdfe59761d6521469
Gabriel <grossy.sjb@gmail.com>
2023-06-26 08:59:56 -0700

    task4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,10 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, b;
-	
-	b = n - 1;
+	int i;
+	int b;
 
+	b = n - 1;
 	for (i = 0; i < n / 2; i++)
 	{
 		int start, end;
@@ -23,5 +23,6 @@ void reverse_array(int *a, int n)
 		end = a[b];
 		a[i] = end;
 		a[b] = start;
+		b--;
 	}
 }
