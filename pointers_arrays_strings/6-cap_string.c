3728338e3922f5ce33741e3476f37be42161d6a2
Gabriel <grossy.sjb@gmail.com>
2023-06-27 06:19:53 -0700

    task6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,16 @@
 
 char *cap_string(char *s)
 {
-	int i;
+	int i, a;
 	char Array[10] = {',', ';', '.', '!', '?', '(', ')', '{', '}', '"'};
+	
+	for (a = 0; Array[a] <= 10; a++)
+		;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == ',' + 1)
+		if (s[i] == a)
 			s -= 32;
 	}
+	return (s);
 }
