a6751d3a921c5c3ce8099d3d042be6e926bd9002
Gabriel <grossy.sjb@gmail.com>
2023-06-28 09:46:20 -0700

    task4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * pow_recursion - function
+ * _pow_recursion - function
  * @x: int 1
  * @y: int 2
  * Return: int
  */
 
-int pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
