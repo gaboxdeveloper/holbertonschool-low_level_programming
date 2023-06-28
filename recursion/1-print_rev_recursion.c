433091617162abd80ecd3a2340fe3024a1e81c56
Gabriel <grossy.sjb@gmail.com>
2023-06-28 07:15:47 -0700

    task1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,12 +7,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s < 0)
+	if (*s == '\0')
 	{
-		_putchar(10);
-		return;
+		_putchar(*s);
 	}
-	_putchar(*s);
-	s = s - 1;
-	_print_rev_recursion(s);
+	_print_rev_recursion(s + 1);
+	_putchar(10);
 }
