e3a97303d6bd643e1ffc22271976dd8b1d00d1f9
Gabriel <grossy.sjb@gmail.com>
2023-06-30 06:38:35 -0700

    task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,19 +8,18 @@
  * Return: 0
  */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int c;
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 
-	c = a * b;
-	printf("%d", c);
-
-	if (argv[1] || argv[2])
+	if (argc < 2)
 	{
 		printf("Error");
 		return (1);
 	}
+	c = a * b;
+	printf("%d", c);
 	return (0);
 }
