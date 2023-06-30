3249a3aaf6a909c9cdb64d84b3ed100e10170f0e
Gabriel <grossy.sjb@gmail.com>
2023-06-30 05:59:12 -0700

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
