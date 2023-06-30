a84c25c6d53ed71f2cba80fe293e6f575458342e
Gabriel <grossy.sjb@gmail.com>
2023-06-30 05:39:18 -0700

    task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 /**
- *
+ * main - function
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
  */
 
-int main (int argc)
+int main(int argc, char *argv[])
 {
 	printf("%d\n", argc);
 	return (0);
