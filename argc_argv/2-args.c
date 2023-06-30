14bba0bfb9dd3e4b446adf845c10b35e37d95ddb
Gabriel <grossy.sjb@gmail.com>
2023-06-30 06:07:13 -0700

    task2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -7,11 +7,11 @@
  * Return: 0
  */
 
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i <= argc; i++)
-		printf("%d\n", argc);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
