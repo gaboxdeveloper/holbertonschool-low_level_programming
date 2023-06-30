abf4821767aaee8a4974159620c63631f806a431
Gabriel <grossy.sjb@gmail.com>
2023-06-30 05:52:03 -0700

    task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -8,8 +7,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%s\n", argv[0]);
 	return (0);
 }
