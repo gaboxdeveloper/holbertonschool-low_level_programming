aa739eb481a8a64eb2f7762a9168628bad9563b0
Gabriel <grossy.sjb@gmail.com>
2023-06-30 05:56:49 -0700

    task1

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,8 +7,11 @@
  * Return: 0
  */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%d\n", argv[i]);
 	return (0);
 }
