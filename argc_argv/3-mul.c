e7955bf258d4000d9c31f6f264b0f0ebca147a63
Gabriel <grossy.sjb@gmail.com>
2023-06-30 06:57:51 -0700

    task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,14 +10,14 @@
 
 int main(int argc, char *argv[])
 {
+	int c, a, b;
 	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int c;
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 
 	c = a * b;
 	printf("%d\n", c);
