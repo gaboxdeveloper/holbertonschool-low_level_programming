3582d71cfd4fa8e416fd7de2aae9cb9706fd339f
Gabriel <grossy.sjb@gmail.com>
2023-07-06 10:34:25 -0700

    task2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * str_concat - function
+ * @s1: fasd
+ * @s2: asdf
+ * Return: char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int s12, a, b;
+
+	s12 = 0;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	for (a = 0; s1[a]; a++)
+		s12++;
+	for (b = 0; s2[b]; b++)
+		s12++;
+	concat = malloc(sizeof(char) * s12);
+	strcat(concat, s1);
+	strcat(concat, s2);
+
+	if (concat == NULL)
+		return (NULL);
+
+	return (concat);
+}
