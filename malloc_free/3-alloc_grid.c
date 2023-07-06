c4c6ebc03313400103ff9967b6227fcb0e900a7c
Gabriel <grossy.sjb@gmail.com>
2023-07-06 11:15:22 -0700

    task3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,29 +10,36 @@
 
 int **alloc_grid(int width, int height)
 {
-	if (width <= 0 || height <= 0) {
-        return NULL;
-    }
+	int **twoD;
+	int hgt_index, wid_index;
 
-    int **grid = (int **)malloc(height * sizeof(int *));
-    if (grid == NULL) {
-        return NULL;
-    }
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    for (int i = 0; i < height; i++) {
-        grid[i] = (int *)malloc(width * sizeof(int));
-        if (grid[i] == NULL) {
-            for (int j = 0; j < i; j++) {
-                free(grid[j]);
-            }
-            free(grid);
-            return NULL;
-        }
+	twoD = malloc(sizeof(int *) * height);
 
-        for (int j = 0; j < width; j++) {
-            grid[i][j] = 0;
-        }
-    }
+	if (twoD == NULL)
+		return (NULL);
 
-    return grid;
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+
+		if (twoD[hgt_index] == NULL)
+		{
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
+
+			free(twoD);
+			return (NULL);
+		}
+	}
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
+	}
+
+	return (twoD);
 }
