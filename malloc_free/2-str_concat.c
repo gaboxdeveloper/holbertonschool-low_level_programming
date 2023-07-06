b4baf42adf3e9502015df7b65fe34bf8139176cb
Gabriel <grossy.sjb@gmail.com>
2023-07-06 10:44:49 -0700

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,8 +16,10 @@ char *str_concat(char *s1, char *s2)
 
 	s12 = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (a = 0; s1[a]; a++)
 		s12++;
