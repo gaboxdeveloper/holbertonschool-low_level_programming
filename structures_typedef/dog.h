3feea1c3a8ea582305d763ad82856316657cde3f
Gabriel <grossy.sjb@gmail.com>
2023-07-10 10:19:02 -0700

    asd

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - asf
+ * @name: of the dog
+ * @age: of the dog
+ * @owner: of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog
+{
+	
+}
+
+#endif
