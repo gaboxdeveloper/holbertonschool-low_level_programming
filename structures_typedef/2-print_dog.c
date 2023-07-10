f30a4942c6beb5a754a156161513b8108170b801
Gabriel <grossy.sjb@gmail.com>
2023-07-10 10:51:51 -0700

    task2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 
 	if (d->age <= 0)
-		printf("Age: 0.0000\n");
+		printf("Age: 0.000000\n");
 	else
 		printf("Age: %f\n", d->age);
 
