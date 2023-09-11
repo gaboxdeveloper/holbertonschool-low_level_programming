bbf40a87445572e2c5d5f8fb4998ab786b4de61d
Gabriel Rossy <grossy.sjb@gmail.com>
2023-09-11 19:48:08 +0000

    task2

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -6,6 +6,7 @@ binary_search(int *array, size_t size, int value)
 	size_t inicio = 0;
 	size_t fin = size - 1;
 	size_t i, medio;
+	int isPresent = 0;
 
 	if (array == NULL || size == 0)
 		return (-1);
@@ -14,7 +15,7 @@ binary_search(int *array, size_t size, int value)
 	{
 		medio = inicio + (fin - inicio) / 2;
 
-		printf("Searching in subarray: ");
+		printf("Searching in array: ");
 		for (i = inicio; i <= fin; i++)
 			if (i != fin)
 				printf("%d, ", array[i]);
@@ -24,12 +25,17 @@ binary_search(int *array, size_t size, int value)
 		printf("\n");
 
 		if (array[medio] == value)
+		{
+			isPresent = 1;
 			return (medio);
+		}
 		if (array[medio] < value)
 			inicio = medio + 1;
 		else
 			fin = medio - 1;
 	}
-
-	return (-1);
+	if (isPresent == 0)
+		return (-1);
+	else
+		return (0);
 }
