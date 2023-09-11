1005ed73454c751093af639a0cb8714c5baa7aab
Gabriel Rossy <grossy.sjb@gmail.com>
2023-09-11 16:14:49 +0000

    first commit and first task

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+int
+linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	int isPresent;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (value == array[i])
+		{
+			isPresent = 1;
+			return (i);
+		}
+		else
+			isPresent = 0;
+	}
+	if (isPresent == 0)
+		return (-1);
+}
